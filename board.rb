4f67b0e7c8abbd870590abc7779657e12ab158ee
hangmanandhide <9014584+hangmanandhide@users.noreply.github.com>
2019-07-12 22:28:33 -0700

    Updates made to tile/board.rb

--- board.rb
+++ board.rb
@@ -1,5 +1,6 @@
 class Board
 
+}
   def self.empty_grid
     grid = Array.new(9) { Array.new(9) }
   end
--- tile.rb
+++ tile.rb
@@ -1,5 +1,18 @@
 class Tile
+  POS = [
+    [-1,1],
+    [0,1],
+    [1,1],
+    [-1,0],
+    [1,0],
+    [-1,-1],
+    [0,-1],
+    [1,-1]
+  ]
 
+  def initialize(board, pos)
+    
+  end
   #reveal
   #neighbors
   #neighbor_bomb_count
